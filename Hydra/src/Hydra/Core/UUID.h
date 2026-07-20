aed59ebf9e235a4c280ae9ade3416d562607eca3
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-07-20 21:58:07 +0400

    refactor(core): simplify std::hash specialization for UUID

--- Hydra/src/Hydra/Core/UUID.h
+++ Hydra/src/Hydra/Core/UUID.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <cstdint>
-#include <functional>
 
 namespace Hydra 
 {
@@ -23,12 +21,14 @@ namespace Hydra
 namespace std 
 {
 
+	template<typename T> struct hash;
+
 	template<>
 	struct hash<Hydra::UUID>
 	{
 		std::size_t operator()(const Hydra::UUID& uuid) const
 		{
-			return hash<uint64_t>()((uint64_t)uuid);
+			return (uint64_t)uuid;
 		}
 	};
 
