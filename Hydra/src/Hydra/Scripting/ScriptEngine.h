98b70df0d6bd55838a1f274318af0ff649d1ea0b
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-08-09 23:30:51 +0400

    feat(script): add ScriptFieldInstance for data storage and value manipulation

--- Hydra/src/Hydra/Scripting/ScriptEngine.h
+++ Hydra/src/Hydra/Scripting/ScriptEngine.h
@@ -38,6 +38,38 @@ namespace Hydra
 		MonoClassField* ClassField;
 	};
 
+	// ScriptField + data storage
+	struct ScriptFieldInstance
+	{
+		ScriptField Field;
+
+		ScriptFieldInstance()
+		{
+			memset(m_Buffer, 0, sizeof(m_Buffer));
+		}
+
+		template<typename T>
+		T GetValue()
+		{
+			static_assert(sizeof(T) <= 8, "Type too large!");
+			return *(T*)m_Buffer;
+		}
+
+		template<typename T>
+		void SetValue(T value)
+		{
+			static_assert(sizeof(T) <= 8, "Type too large!");
+			memcpy(m_Buffer, &value, sizeof(T));
+		}
+	private:
+		uint8_t m_Buffer[8];
+
+		friend class ScriptEngine;
+		friend class ScriptInstance;
+	};
+
+	using ScriptFieldMap = std::unordered_map<std::string, ScriptFieldInstance>;
+
     class ScriptClass
 	{
 	public:
@@ -75,7 +107,9 @@ namespace Hydra
 		template<typename T>
 		T GetFieldValue(const std::string& name)
 		{
-			bool success = GetFieldValueInternal(name, s_FieldValueBuffer);
+			static_assert(sizeof(T) <= 8, "Type too large!");
+
+			bool success = GetFieldValueInternal(name, s_FieldValueBuffer);	
 			if (!success)
 				return T();
 
@@ -83,14 +117,16 @@ namespace Hydra
 		}
 
 		template<typename T>
-		void SetFieldValue(const std::string& name, const T& value)
+		void SetFieldValue(const std::string& name, T value)
 		{
+			static_assert(sizeof(T) <= 8, "Type too large!");
+
 			SetFieldValueInternal(name, &value);
 		}
 
 	private:
 		bool GetFieldValueInternal(const std::string& name, void* buffer);
-		bool SetFieldValueInternal(const std::string& name, const	void* value);
+		bool SetFieldValueInternal(const std::string& name, const void* value);
 
 	private:
 		Ref<ScriptClass> m_ScriptClass;
@@ -101,6 +137,9 @@ namespace Hydra
 		MonoMethod* m_OnUpdateMethod = nullptr;
 
 		inline static char s_FieldValueBuffer[8];
+
+		friend class ScriptEngine;
+		friend struct ScriptFieldInstance;
 	};
 
 	
@@ -123,7 +162,9 @@ namespace Hydra
 		static Scene* GetSceneContext();
 		static Ref<ScriptInstance> GetEntityScriptInstance(UUID entityID);
 		
+		static Ref<ScriptClass> GetEntityClass(const std::string& name);
 		static std::unordered_map<std::string, Ref<ScriptClass>> GetEntityClasses();
+		static ScriptFieldMap& GetScriptFieldMap(Entity entity);
 		
 		static MonoImage* GetCoreAssemblyImage();
 		
